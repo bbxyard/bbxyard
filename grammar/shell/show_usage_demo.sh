6039fad7b53ec6c4ade63b938e5ca89a979bf331
bbxyard <bbxyard@gmail.com>
2015-12-01 17:01:43 +0800

    .

--- grammar/shell/show_usage_demo.sh
+++ grammar/shell/show_usage_demo.sh
@@ -4,7 +4,7 @@
 AUTHOR=bbxyard@gmail.com
 COPY_RIGHT=
 
-show_usage()
+function show_usage()
 {
     cat << EOF
 Usage: show_usage_demo [OPTION]
@@ -16,6 +16,7 @@ Known values for OPTIONS are:
   --includedir      print location where headers are installed
   --ldflags
   --libs            print library information
+  --outfile -o      output file  
   ...
   --help            print this help
 
@@ -24,32 +25,62 @@ write by $AUTHOR.
 EOF
 }
 
-if test $# -eq 0; then
-    show_usage
-    exit 2
-fi
+function write_to_file()
+{
+    cat >> $1 << EOF
+Usage: show_usage_demo [OPTION]
 
-while test $# -gt 0; do
+Known values for OPTIONS are:
+  --prefix[=DIR]    change prefix to DIR
+  --bindir          print location where binaries are installed
+  --includes        print include information
+  --includedir      print location where headers are installed
+  --ldflags
+  --libs            print library information
+  --outfile -o      output file
+  ...
+  --help            print this help
 
-  case "$1" in
-  --prefix)
-  echo show prefix
+Copyright (C) 2013 Free Software Foundation, Inc.
+write by $AUTHOR.
+EOF
+}
+
+function main()
+{
+  if test $# -eq 0; then
+      show_usage
+      exit 2
+  fi
+
+  while test $# -gt 0; do
+    case "$1" in
+      --prefix)
+        echo show prefix
+        exit 0
+        ;;
+      --includes)
+        echo show includes
+        exit 0
+        ;;
+      --outfile|-o)
+        echo show outfile
+        write_to_file "$2"
+        exit 0
+        ;;
+      --help)
+        show_usage
+        exit 0
+        ;;
+      *)
+        show_usage
+        exit 1
+        ;;
+    esac
+  done
+  
   exit 0
-  ;;
-  --includes)
-  echo show includes
-  exit 0
-  ;;
-  --help)
-  show_usage
-  exit 0
-  ;;
-  *)
-  show_usage
-  exit 1
-  ;;
-  esac
-done
+}
 
-exit 0
 
+main $@
