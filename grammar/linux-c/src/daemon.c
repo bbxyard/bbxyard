9d68e224be9f7f70bd381f88ae0d35aa02590c76
bbxyard <bbxyard@gmail.com>
2016-01-11 21:12:02 +0800

    daemon.c stdard

--- grammar/linux-c/src/daemon.c
+++ grammar/linux-c/src/daemon.c
@@ -1,19 +1,39 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <syslog.h>
 
-void daemonize(void)
+
+static int run(int argc, char* argv[])
 {
+	while (1)
+	{
+		syslog(LOG_CRIT, "sub <%s> running!", argv[0]);
+		sleep(1);
+	}
+	return 0;
+}
+
+static int daemonize(int argc, char* argv[])
+{
+	int ret = 0;
 	pid_t  pid;
 
 	/*
 	 * Become a session leader to lose controlling TTY.
 	 */
-	if ((pid = fork()) < 0) {
+	if ((pid = fork()) < 0)
+	{
 		perror("fork");
 		exit(1);
-	} else if (pid != 0) /* parent */
-		exit(0);
+	}
+	else if (pid != 0) /* parent */
+	{
+		fprintf(stdout, "parent exited!!\n");
+		exit(ret);
+	}
+
+	/* sub process */
 	setsid();
 
 	/*
@@ -22,7 +42,7 @@ void daemonize(void)
 	if (chdir("/") < 0) {
 		perror("chdir");
 		exit(1);
-	} 
+	}
 
 	/*
 	 * Attach file descriptors 0, 1, and 2 to /dev/null.
@@ -31,11 +51,14 @@ void daemonize(void)
 	open("/dev/null", O_RDWR);
 	dup2(0, 1);
 	dup2(0, 2);
+
+	/* sub process run */
+	ret = run(argc, argv);
+	return ret;
 }
 
-int main(void)
+int main(int argc, char* argv[])
 {
-	daemonize();
-	while(1);
+	int ret = daemonize(argc, argv);
+	return ret;
 }
-
