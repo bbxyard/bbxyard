d95f9ea8991abae4eb86d678ace59b6825d76b18
bbxyard <bbxyard@gmail.com>
2016-01-20 21:23:38 +0800

    add dateUtil

--- yard/workspace/hmongo/src/main/java/com/bbxyard/util/Util.java
+++ yard/workspace/hmongo/src/main/java/com/bbxyard/util/Util.java
@@ -1,5 +1,8 @@
 package com.bbxyard.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 
 public class Util {
@@ -14,4 +17,28 @@ public class Util {
 		}
 		return map;
 	}
+	
+	// e.g. sfmt "yyyy-MM-dd HH:mm:ss"
+    public static String getCurrentDataTime(String sfmt) {
+        // 格式化当前日期时间
+        Calendar c1 = Calendar.getInstance();
+        c1.setTime(new Date());
+        SimpleDateFormat sdf = new SimpleDateFormat(sfmt);
+        String now = sdf.format(c1.getTime());
+        return now;
+    }
+
+//    public static String getClientAddr(HttpServletRequest request) {
+//        String ip = request.getHeader("x-forwarded-for");
+//        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+//            ip = request.getHeader("Proxy-Client-IP");
+//        }
+//        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+//            ip = request.getHeader("WL-Proxy-Client-IP");
+//        }
+//        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+//            ip = request.getRemoteAddr();
+//        }
+//        return ip;
+//    }	
 }
