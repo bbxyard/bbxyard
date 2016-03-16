45c6e5382d16134d838acd3317e36c83e151cd7b
bbxyard <bbxyard@gmail.com>
2016-03-16 16:55:38 +0800

    add idea

--- yard/toolkits/shell/opendown.sh
+++ yard/toolkits/shell/opendown.sh
@@ -8,3 +8,9 @@ wget http://mirrors.ustc.edu.cn/gnu/glibc/glibc-2.22.tar.bz2
 
 # reviewboard
 wget https://bitnami.com/download/files/stacks/reviewboard/2.5.2-0/bitnami-reviewboard-2.5.2-0-linux-x64-installer.run
+
+# IDEA
+wget https://d1opms6zj7jotq.cloudfront.net/idea/ideaIU-15.0.4.tar.gz
+wget https://d1opms6zj7jotq.cloudfront.net/cpp/clion-1.2.4.tar.gz
+wget https://d1opms6zj7jotq.cloudfront.net/webide/PhpStorm-10.0.3.tar.gz
+wget https://d1opms6zj7jotq.cloudfront.net/resharper/JetBrains.ReSharperUltimate.10.0.2.exe
