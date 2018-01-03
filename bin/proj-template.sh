5fc861617d9900a06901d3964db90c3e3f66247c
bbxyard <bbxyard@gmail.com>
2018-01-03 20:57:03 +0800

    add vue simple demo

--- bin/proj-template.sh
+++ bin/proj-template.sh
@@ -2,13 +2,13 @@
 # project template
 
 # generate grunt project
-function grunt-gen()
+function xb-grunt-gen()
 {
     dname=$1
     NPM=cnpm
     [ x"$1" == x ] && echo "usage: $0 <dirname>" && return 1;
-    mkdir $dname;
-    pushd $dname
+    mkdir "$dname";
+    pushd "$dname"
       $NPM init
       $NPM install grunt --save-dev
       $NPM install grunt-contrib-jshint   --save-dev
@@ -17,5 +17,21 @@ function grunt-gen()
       $NPM install grunt-contrib-qunit    --save-dev
       $NPM install grunt-contrib-concat   --save-dev
       $NPM install grunt-contrib-watch    --save-dev
+    popd
+}
+
+# generate vue project
+function xb-vue-gen()
+{
+    # for advanced demo
+    # use vue init webpack vue-adv-demo
+    dname=$1
+    NPM=cnpm
+    [ x"$1" == x ] && echo "usage: $0 <project_name>" && return 1;
+    cnpm install -g vue-cli
+    vue init webpack-simple "$dname"
+    pushd "$dname"
+      cnpm install
+      cnpm run dev # ready to go!
     popd
 }
---
+++ yard/skills/vue-simple-demo/src/diy.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="diy">
+    <h1>hello slot demo</h1>
+    <h2> {{msg}} </h2>
+    <children>
+      <span slot="1stx" @click="tobeknow">12345</span>
+      <span slot="2nd">67890</span>
+    </children>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'diy',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js SLOT DEMO'
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+#diy {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: green;
+  margin-top: 30px;
+}
+
+h1, h2 {
+  font-weight: bold;
+}
+</styple>
