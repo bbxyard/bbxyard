2e45e5ef01e13f742d3288b4457122f7742dc507
bbxyard <bbxyard@gmail.com>
2018-02-25 01:09:14 +0800

    wepy add config.js 分离依赖

---
+++ yard/skills/wepy-demo/src/config.js
@@ -0,0 +1,19 @@
+
+
+
+const index = {
+  list: [
+    { id: 'fun', name: '好玩', open: false, pages: ['../pages/fun/wheel'] },
+    { id: 'grammar', name: '基础语法', open: false, pages: ['../pages/gr/for', '../pages/gr/if'] },
+    { id: 'form', name: '表单', open: false, pages: ['../pages/index', '../pages/list', 'input', 'list', 'uploader'] },
+    { id: 'widget', name: '基础组件', open: false, pages: ['article', 'badge', 'button', 'flex', 'grid', 'icons', 'loadmore', 'panel', 'preview', 'progress', 'slider'] },
+    { id: 'feedback', name: '操作反馈', open: false, pages: ['actionsheet', 'dialog', 'msg', 'picker'] },
+    { id: 'nav', name: '导航相关', open: false, pages: ['navbar', 'footer'] },
+    { id: 'search', name: '搜索相关', open: false, pages: ['searchbar'] }
+  ]
+};
+
+
+module.exports = {
+  index: index
+};
