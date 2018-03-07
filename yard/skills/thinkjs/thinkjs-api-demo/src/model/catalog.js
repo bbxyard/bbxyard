07e8e55d108739c3dff45daa28a3da00e8256de8
bbxyard <bbxyard@gmail.com>
2018-03-07 17:17:44 +0800

    thinkjs model added done

--- yard/skills/thinkjs/thinkjs-api-demo/src/model/catalog.js
+++ yard/skills/thinkjs/thinkjs-api-demo/src/model/catalog.js
@@ -1,3 +1,12 @@
 module.exports = class extends think.Model {
+  async getChildCategoryId(parentId) {
+    const childIds = await this.where({parent_id: parentId}).getField('id', 10000);
+    return childIds;
+  }
 
+  async getCategoryWhereIn(catId) {
+    const childIds = this.getChildCategoryId(catId);
+    childIds.push(catId);
+    return childIds;
+  }
 };
