cfebec691f2b363c0f95eb99eb06b35dbccaa7bd
bbxyard <bbxyard@gmail.com>
2019-04-19 17:15:04 +0800

    Update index.php
    调色

--- yard/grammar/php/b4/index.php
+++ yard/grammar/php/b4/index.php
@@ -10,8 +10,9 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
-
-    <nav class="navbar navbar-expand-md navbar-dark" style="background-color: #e4685d;">
+    <!-- color: #34495e -->
+    <!-- <nav class="navbar navbar-expand-md navbar-dark" style="background-color: #e4685d;"> -->
+    <nav class="navbar navbar-expand-md navbar-dark" style="background-color: #32b1af;">
         <a class="navbar-brand" href="//me/gm/php/b4">PHP</a>
         <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
             aria-expanded="false" aria-label="Toggle navigation"></button>
