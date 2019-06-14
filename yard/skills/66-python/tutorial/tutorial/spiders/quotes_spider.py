f13382d28f429dc42f02500e4d9adf7da4f49125
bbxyard <bbxyard@gmail.com>
2019-06-14 17:43:42 +0800

    + python 爬虫样例

---
+++ yard/skills/66-python/tutorial/tutorial/spiders/quotes_spider.py
@@ -0,0 +1,26 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+  name = "quotes"
+
+  start_urls = [
+    'http://quotes.toscrape.com/page/1/',
+    'http://quotes.toscrape.com/page/2/',
+  ]
+
+  def parse(self, response):
+    for quote in response.css('div.quote'):
+      yield {
+        'text': quote.css('span.text::text').get(),
+        'author': quote.css('small.author::text').get(),
+        'tags': quote.css('div.tags a.tag::text').getall()
+      }
+
+  def parse2(self, response):
+    page = response.url.split("/")[-2]
+    filename = 'quotes-%s.html' % page
+    with open(filename, 'wb') as f:
+      f.write(response.body)
+    self.log('Saved file %s' % filename)
+
