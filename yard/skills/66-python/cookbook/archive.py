52a4252ab68f5617fc08d8bcc38730f9ad7f7688
bbxyard <bbxyard@gmail.com>
2019-06-18 02:58:36 +0800

    add xlsx 写入

--- yard/skills/66-python/cookbook/archive.py
+++ yard/skills/66-python/cookbook/archive.py
@@ -5,6 +5,9 @@ import os
 import shutil
 
 class ArchiveMan:
+  def info(self):
+    print('archive types: ', shutil.get_archive_formats())
+
   def archive(self, dir, out_dir_path, out_file_basename):
     if not os.path.exists(out_dir_path):
       os.mkdir(out_dir_path)
@@ -12,16 +15,17 @@ class ArchiveMan:
     out_file_prefix = out_dir_path + '/' + out_file_basename
     shutil.make_archive(out_file_prefix, 'zip', dir)
     shutil.make_archive(out_file_prefix, 'tar', dir)
-    # shutil.make_archive(out_file_prefix, 'tar.gz', dir)
-    # shutil.make_archive(out_file_prefix, 'tar.bz2', dir)
+    shutil.make_archive(out_file_prefix, 'gztar', dir)
+    shutil.make_archive(out_file_prefix, 'bztar', dir)
 
-  def unpack(self):
-    pass
+  def unpack(self, in_ar_file, out_dir):
+    shutil.unpack_archive(in_ar_file, out_dir)
 
 
 def main():
   ar = ArchiveMan()
   ar.archive('/usr/local/etc', 'out', 'usr-local-etc')
+  ar.unpack('out/usr-local-etc.tar', 'out/usr-local-etc')
   print('done')
 
 
---
+++ yard/skills/66-python/cookbook/io_fmt_csv.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+#
+
+import pandas as pd
+
+def read_passwd_file(file='/etc/passwd'):
+  out = []
+  with open(file, 'r') as fp:
+    lines = fp.readlines()
+    for ln in lines:
+      ln = ln.strip('\n')
+      row = ln.split(':')
+      if len(row) >= 7:
+        out.append(row)
+      # print(row)
+  return out
+
+
+def write_csv_by_pandas(mat, outfile):
+  names, uids, gids = [], [], []
+  num = 0
+  for row in mat:
+    print(row, len(row), num)
+    num += 1
+    # if len(row) >= 7:
+    names.append(row[0])
+    uids.append(row[2])
+    gids.append(row[3])
+  # 组装数据
+  _data = pd.DataFrame()
+  _data['用户名'] = names
+  _data['用户id'] = uids
+  _data['组id'] = gids
+  # # 写入文件
+  pd.concat([_data]).reset_index(drop=True).to_csv(outfile, index=False, encoding='utf-8')
+
+
+def write_xlsx_by_openpyxl(mat, outfile):
+  from openpyxl import Workbook
+  wb = Workbook()
+  ws1 = wb.active
+  ws1.title = 'passwd'
+
+  ws1['A1'] = '用户名'
+  ws1['B1'] = '用户id'
+  ws1['C1'] = '组id'
+
+  row_cnt = len(mat)
+  i = 1
+  for row in mat:
+    ws1['A%s' % (i + 1)] = row[0]
+    ws1['B%s' % (i + 1)] = row[2]
+    ws1['C%s' % (i + 1)] = row[3]
+    i += 1
+  wb.save(outfile)
+
+
+def main():
+  mat = read_passwd_file()
+  write_csv_by_pandas(mat, 'out/etc-passwd.csv')
+  write_xlsx_by_openpyxl(mat, 'out/etc-passwd.xlsx')
+
+
+if __name__ == "__main__":
+  main()
