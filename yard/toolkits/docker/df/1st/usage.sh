40b06ba26331d7c82c296cafecd2658d080d4bcc
bbxyard <bbxyard@gmail.com>
2019-07-11 17:12:27 +0800

    docker usage

--- yard/toolkits/docker/df/1st/usage.sh
+++ yard/toolkits/docker/df/1st/usage.sh
@@ -1,5 +1,13 @@
 # 导入
-docker build --rm=true -t "apk-bash${TAG}" - < "../df/apk-bash.dockerfile"
-docker build --rm=true -t "apk-node${TAG}" - < "../df/apk-node.dockerfile"
-docker build --rm=true -t "os-u18${TAG}" - < "../df/u18-init.dockerfile"
-docker build --rm=true -t "os-c7${TAG}" - < "../df/c7-init.dockerfile
+
+# docker build --rm=true -t "os-c7${TAG:-:0.1}" - < "../df/1st/os-centos.dockerfile"
+
+docker build --rm=true -t "os-apk${TAG:-:0.1}" https://raw.githubusercontent.com/0a0a/docker/master/df/1st/os-alpine.dockerfile
+docker build --rm=true -t "os-u18${TAG:-:0.1}" https://raw.githubusercontent.com/0a0a/docker/master/df/1st/os-ubuntu.dockerfile
+docker build --rm=true -t "os-c7${TAG:-:0.1}" https://raw.githubusercontent.com/0a0a/docker/master/df/1st/os-centos.dockerfile
+
+docker build --rm=true -t "bash-apk${TAG:-:0.1}" https://raw.githubusercontent.com/0a0a/docker/master/df/1st/bash-alpine.dockerfile
+
+docker build --rm=true -t "node-apk-dev${TAG:-:0.1}" https://raw.githubusercontent.com/0a0a/docker/master/df/1st/node-alpine-dev.dockerfile
+docker build --rm=true -t "node-u18-dev${TAG:-:0.1}" https://raw.githubusercontent.com/0a0a/docker/master/df/1st/node-ubuntu-dev.dockerfile
+# docker build --rm=true -t "node-c7-dev${TAG:-:0.1}" https://raw.githubusercontent.com/0a0a/docker/master/df/1st/node-centos-dev.dockerfile
