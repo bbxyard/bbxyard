b491ad39eeb77dc8e11d0a13f22a6eb1bcc9030b
bbxyard <bbxyard@gmail.com>
2019-07-12 23:25:09 +0800

    增加定时任务

--- yard/skills/00-dss/celery-demo/celery_app/celeryconfig.py
+++ yard/skills/00-dss/celery-demo/celery_app/celeryconfig.py
@@ -1,3 +1,6 @@
+from datetime import timedelta
+from celery.schedules import crontab
+
 BROKER_URL = "redis://localhost:9999/1"
 
 CELERY_RESULT_BACKEND = "redis://localhost:9999/2"
@@ -9,4 +12,19 @@ CELERY_TIMEZONE = "Asia/Shanghai"
 CELERY_IMPORTS = {
     'celery_app.task1',
     'celery_app.task2'
+}
+
+
+# 定时任务
+CELERYBEAT_SCHEDULE = {
+    "task1": {
+        "task": "celery_app.task1.add",
+        "schedule": timedelta(seconds=10),
+        "args": (2, 8)
+    },
+    "task2": {
+        "task": "celery_app.task2.multiply",
+        "schedule": crontab(hour=23, minute=20),
+        "args": (4, 5)
+    }
 }
