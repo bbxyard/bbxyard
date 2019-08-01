bace35781eb5f93f8c5b186ed2cf881f7fb775a6
bbxyard <bbxyard@gmail.com>
2019-08-01 14:59:21 +0800

    add netdata

---
+++ yard/toolkits/docker/usage/netdata-ctl.mk
@@ -0,0 +1,34 @@
+# netdata 管理
+
+DK_RUN  := docker run --rm --privileged -v/etc/localtime:/etc/localtime -v/home:/home
+
+TAG     := netdata-c7:0.1
+NAME    := netdata
+
+
+# ####################################
+# Dashboard AREA
+# ####################################
+start:
+	$(DK_RUN) -d -h $(NAME) --name $(NAME) -p19999:19999 $(TAG) 
+stop:
+	docker stop $(NAME)
+bash:
+	docker exec -it $(NAME) /bin/bash
+status:
+	docker ps -a
+
+
+# ####################################
+# Init AREA
+# ####################################
+build:
+	docker build -t $(TAG) https://raw.githubusercontent.com/0a0a/docker/master/df/dashboard/netdata-centos.dockerfile
+
+
+# ####################################
+# Fini AREA
+# ####################################
+clean:
+	docker rmi $(TAG)
+	rm -rvf *.bak *.log
