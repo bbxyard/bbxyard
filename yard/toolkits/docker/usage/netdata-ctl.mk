53b0e993046b7ae42d4debe98bf85317307d9003
bbxyard <bbxyard@gmail.com>
2019-08-01 16:02:27 +0800

    Update netdata-ctl.mk
    标准化

--- yard/toolkits/docker/usage/netdata-ctl.mk
+++ yard/toolkits/docker/usage/netdata-ctl.mk
@@ -1,31 +1,45 @@
 # netdata 管理
 
+# 固定参数部分
 DK_RUN  := docker run --rm --privileged -v/etc/localtime:/etc/localtime -v/home:/home
 
-TAG     := netdata-c7:0.1
-NAME    := netdata
+# 变化参数部分
+TAG       := netdata-c7:0.1
+NAME      := netdata
+DF_URL    := https://raw.githubusercontent.com/0a0a/docker/master/df/dashboard/netdata-centos.dockerfile
+START_CMD := 
+DK_V_OPT  := -p19999:19999
 
 
 # ####################################
 # Dashboard AREA
 # ####################################
-start:
-	$(DK_RUN) -d -h $(NAME) --name $(NAME) -p19999:19999 $(TAG)
-	docker ps -a
-stop:
-	docker stop $(NAME)
+start: do-start do-show-ps
+stop: do-stop do-show-ps do-show-images
+status: do-show-ps
 bash:
 	docker exec -it $(NAME) /bin/bash
-status:
+
+do-start:
+	$(DK_RUN) -d -h $(NAME) --name $(NAME) $(DK_V_OPT) $(TAG) $(START_CMD)
+do-stop:
+	docker stop $(NAME)
+do-show-ps:
+	@echo
 	docker ps -a
+	@echo
+do-show-images:
+	@echo
+	docker images
+	@echo
 
 
 # ####################################
 # Init AREA
 # ####################################
-build:
-	docker build -t $(TAG) https://raw.githubusercontent.com/0a0a/docker/master/df/dashboard/netdata-centos.dockerfile
-	docker images
+build: do-build do-show-images
+do-build:
+	docker build -t $(TAG) $(DF_URL)
 
 
 # ####################################
