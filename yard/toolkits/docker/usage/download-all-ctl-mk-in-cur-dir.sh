3cbad0bb234cb34dad85fd512ea5fdc095678148
bbxyard <bbxyard@gmail.com>
2019-08-01 16:56:36 +0800

    update ctrl script

--- yard/toolkits/docker/usage/download-all-ctl-mk-in-cur-dir.sh
+++ yard/toolkits/docker/usage/download-all-ctl-mk-in-cur-dir.sh
@@ -1,7 +1,7 @@
-curl https://raw.githubusercontent.com/0a0a/docker/master/usage/netdata-ctl.mk -o netdata-ctl.mk
-curl https://raw.githubusercontent.com/0a0a/docker/master/usage/node-apk-dev-ctl.mk -o node-apk-dev-ctl.mk
-curl https://raw.githubusercontent.com/0a0a/docker/master/usage/node-u18-dev-ctl.mk -o node-u18-dev-ctl.mk
-curl https://raw.githubusercontent.com/0a0a/docker/master/usage/os-alpine-ctl.mk -o os-alpine-ctl.mk
-curl https://raw.githubusercontent.com/0a0a/docker/master/usage/os-apk-dev-ctl.mk -o os-apk-dev-ctl.mk
-curl https://raw.githubusercontent.com/0a0a/docker/master/usage/os-c7-ctl.mk -o os-c7-ctl.mk
-curl https://raw.githubusercontent.com/0a0a/docker/master/usage/os-u18-ctl.mk -o os-u18-ctl.mk
+curl -skSL https://raw.githubusercontent.com/0a0a/docker/master/usage/netdata-ctl.mk -o netdata-ctl.mk
+curl -skSL https://raw.githubusercontent.com/0a0a/docker/master/usage/node-apk-dev-ctl.mk -o node-apk-dev-ctl.mk
+curl -skSL https://raw.githubusercontent.com/0a0a/docker/master/usage/node-u18-dev-ctl.mk -o node-u18-dev-ctl.mk
+curl -skSL https://raw.githubusercontent.com/0a0a/docker/master/usage/os-alpine-ctl.mk -o os-alpine-ctl.mk
+curl -skSL https://raw.githubusercontent.com/0a0a/docker/master/usage/os-apk-dev-ctl.mk -o os-apk-dev-ctl.mk
+curl -skSL https://raw.githubusercontent.com/0a0a/docker/master/usage/os-c7-ctl.mk -o os-c7-ctl.mk
+curl -skSL https://raw.githubusercontent.com/0a0a/docker/master/usage/os-u18-ctl.mk -o os-u18-ctl.mk
