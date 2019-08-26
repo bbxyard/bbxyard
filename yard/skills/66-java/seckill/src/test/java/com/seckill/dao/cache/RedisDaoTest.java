83d2be19d0ab6166d2c1b2d333faf30de86cef55
bbxyard <bbxyard@gmail.com>
2019-08-26 23:19:43 +0800

    RedisDao 单元测试

---
+++ yard/skills/66-java/seckill/src/test/java/com/seckill/dao/cache/RedisDaoTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2019 study code from imooc.
+ *
+ * https://www.imooc.com/learn/587
+ *
+ * Java高并发秒杀API之业务分析与DAO层
+ *
+ */
+
+package com.seckill.dao.cache;
+
+import com.seckill.dao.SeckillDao;
+import com.seckill.entity.Seckill;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/spring-dao.xml"})
+public class RedisDaoTest {
+
+    // 日志对象
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private long id = 1002;
+
+    @Autowired
+    private RedisDao redisDao;
+
+    @Autowired
+    private SeckillDao seckillDao;
+
+    @Test
+    public void testSeckill() {
+        Seckill seckill = redisDao.getSeckill(id);
+        if (seckill == null) {
+            seckill = seckillDao.queryById(id);
+            if (seckill != null) {
+                String result = redisDao.putSeckill(seckill);
+                System.out.println("Put to Redis: " + result);
+                seckill = redisDao.getSeckill(id);
+                System.out.println("Get from Redis: " + seckill);
+                logger.info("seckill={}", seckill);
+            }
+        }
+    }
+}
---
+++ yard/skills/66-java/seckill/src/test/java/com/seckill/service/SeckillServiceTest.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2019 study code from imooc.
+ *
+ * https://www.imooc.com/learn/587
+ *
+ * Java高并发秒杀API之业务分析与DAO层
+ *
+ */
+
+package com.seckill.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SeckillServiceTest {
+
+    @Test
+    public void getSeckillList() {
+    }
+
+    @Test
+    public void getById() {
+    }
+
+    @Test
+    public void exportSeckillUrl() {
+    }
+
+    @Test
+    public void executeSeckill() {
+    }
+
+    @Test
+    public void executeSeckillProcedure() {
+    }
+}
