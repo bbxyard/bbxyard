629b99acd26f2ffdff7bff3fbb5a3a92676d62bf
bbxyard <bbxyard@gmail.com>
2019-08-26 09:54:40 +0800

    集成DaoTest

--- yard/skills/66-java/seckill/src/test/java/com/seckill/dao/SuccessKilledDaoTest.java
+++ yard/skills/66-java/seckill/src/test/java/com/seckill/dao/SuccessKilledDaoTest.java
@@ -9,19 +9,36 @@
 
 package com.seckill.dao;
 
+import com.seckill.entity.SuccessKilled;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.*;
+import javax.annotation.Resource;
 
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/spring-dao.xml"})
 public class SuccessKilledDaoTest {
+
+    @Resource
+    private SuccessKilledDao successKilledDao;
 
     @Test
     public void insertSuccessKilled() {
-
+        long seckillId = 1001L;
+        long userPhone = 13812345678L;
+        int insertCount = successKilledDao.insertSuccessKilled(seckillId, userPhone);
+        System.out.println("Insert Count = " + insertCount);
     }
 
     @Test
     public void queryByIdWithSeckill() {
-
+        long seckillId = 1001L;
+        long userPhone = 13812345678L;
+        SuccessKilled successKilled = successKilledDao.queryByIdWithSeckill(seckillId, userPhone);
+        System.out.println(successKilled);
+        System.out.println(successKilled.getSeckill());
     }
 }
