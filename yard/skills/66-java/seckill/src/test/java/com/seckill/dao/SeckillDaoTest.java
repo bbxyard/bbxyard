aeeb2fe67ee5f12b98411adb907dfde3d0601b06
bbxyard <bbxyard@gmail.com>
2019-08-26 09:44:35 +0800

    test case fix

--- yard/skills/66-java/seckill/src/test/java/com/seckill/dao/SeckillDaoTest.java
+++ yard/skills/66-java/seckill/src/test/java/com/seckill/dao/SeckillDaoTest.java
@@ -9,26 +9,57 @@
 
 package com.seckill.dao;
 
+import com.seckill.entity.Seckill;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+import java.util.Date;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
+/**
+ * 配置Spring和Junit整合,junit启动时加载springIOC容器 spring-test,junit
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+// 告诉JUnit spring配置文件
+@ContextConfiguration({"classpath:spring/spring-dao.xml"})
 public class SeckillDaoTest {
+
+    // 注入Dao实现类依赖
+    @Resource
+    private SeckillDao seckillDao;
 
     @Test
     public void reduceNumber() {
+        long seckillId = 1000;
+        Date date = new Date();
+        int updateCount = seckillDao.reduceNumber(seckillId, date);
+        System.out.println(updateCount);
     }
 
     @Test
     public void queryById() {
+        long seckillId = 1000;
+        Seckill seckill = seckillDao.queryById(seckillId);
+        System.out.printf("name is: %s\n", seckill.getName());
+        System.out.println(seckill);
     }
 
     @Test
     public void queryAll() {
+        List<Seckill> seckills = seckillDao.queryAll(0, 100);
+        for (Seckill seckill : seckills) {
+            System.out.println(seckill);
+        }
     }
 
     @Test
     public void killByProcedure() {
-        
+        System.out.println("Reserved");
     }
 }
