5ba37ea3d6b906f35a8a469aa321226101997cd0
bbxyard <bbxyard@gmail.com>
2019-08-28 15:36:31 +0800

    error直接触发及处理

---
+++ yard/skills/66-java/spboot/src/main/java/com/bbxyard/spboot/exception/IMoocExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.bbxyard.spboot.exception;
+
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class IMoocExceptionHandler {
+
+    public static final String IMOOC_ERROR_VIEW = "error";
+
+    @ExceptionHandler(value = Exception.class)
+    public Object errorHandler(HttpServletRequest request, HttpServletRequest response, Exception e) throws Exception {
+        e.printStackTrace();
+
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exception", e);
+        mav.addObject("url", request.getRequestURL());
+        mav.setViewName(IMOOC_ERROR_VIEW);
+        return mav;
+    }
+}
