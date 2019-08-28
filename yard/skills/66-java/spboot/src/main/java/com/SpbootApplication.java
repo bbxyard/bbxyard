03596982e9c94e679c7ee1c651d0967d7546ccb6
bbxyard <bbxyard@gmail.com>
2019-08-28 20:23:18 +0800

    代码规整

--- yard/skills/66-java/spboot/src/main/java/com/SpbootApplication.java
+++ yard/skills/66-java/spboot/src/main/java/com/SpbootApplication.java
@@ -6,12 +6,11 @@ import org.springframework.context.annotation.ComponentScan;
 import tk.mybatis.spring.annotation.MapperScan;
 
 
+@SpringBootApplication
 // 扫描MyBatis mapper包路径
 @MapperScan(basePackages = "com.imooc.mapper")
 // 扫描所有需要包, 包含一些自用的工具包 所在路径
-@ComponentScan(basePackages = {"com.imooc", "org.n3r.idworker"})
-
-@SpringBootApplication
+@ComponentScan(basePackages = {"com.imooc", "com.bbxyard.spboot", "org.n3r.idworker"})
 public class SpbootApplication {
 
     public static void main(String[] args) {
