d6538121d1cf3fee1391a7ffa7132e81fe9920bf
bbxyard <bbxyard@gmail.com>
2019-08-29 19:21:46 +0800

    thymeleaf 模板深入学习

--- yard/skills/66-java/spboot/src/main/java/com/bbxyard/spboot/controller/ThymeleafController.java
+++ yard/skills/66-java/spboot/src/main/java/com/bbxyard/spboot/controller/ThymeleafController.java
@@ -1,8 +1,13 @@
 package com.bbxyard.spboot.controller;
 
+import com.imooc.pojo.SysUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 
 @Controller
@@ -19,4 +24,38 @@ public class ThymeleafController {
     public String center() {
         return "thymeleaf/center/center";
     }
+
+    @RequestMapping("/test")
+    public String test(ModelMap map) {
+        SysUser sysUser = new SysUser();
+        sysUser.setNickname("Thymeleaf");
+        sysUser.setAge(124);
+        sysUser.setPassword("ABCD1234");
+        sysUser.setLastLoginTime(new Date());
+        sysUser.setAddress("<font color='green'><b>北京市 海淀区 五道口</b></font>");
+        map.addAttribute("user", sysUser);
+
+        List<SysUser> userList = new ArrayList<>();
+        userList.add(sysUser);
+        for (int i = 0; i < 5; ++i) {
+            SysUser user = new SysUser();
+            user.setNickname("Freemarker-" + i);
+            user.setAge(16 + i);
+            user.setPassword("ABCD1234");
+            user.setLastLoginTime(new Date());
+            user.setAddress("北京市 海淀区 五道口");
+            userList.add(user);
+        }
+        map.addAttribute("userList", userList);
+
+        return "thymeleaf/test";
+    }
+
+    @RequestMapping("postform")
+    public String postform(SysUser user) {
+        System.out.println("姓名: " + user.getNickname());
+        System.out.println("年龄: " + user.getAge());
+        return "redirect:/th/test";
+    }
 }
+
