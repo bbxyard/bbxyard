6a62ebcb6d19254268590a5b78400b5424d826ed
bbxyard <bbxyard@gmail.com>
2019-08-29 09:27:24 +0800

    引入事务

--- yard/skills/66-java/spboot/src/main/java/com/imooc/service/impl/SysUserServiceImpl.java
+++ yard/skills/66-java/spboot/src/main/java/com/imooc/service/impl/SysUserServiceImpl.java
@@ -7,6 +7,8 @@ import com.imooc.pojo.SysUser;
 import com.imooc.service.SysUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.thymeleaf.util.StringUtils;
 import tk.mybatis.mapper.entity.Example;
 
@@ -92,6 +94,7 @@ public class SysUserServiceImpl implements SysUserService {
     }
 
     @Override
+    @Transactional(propagation = Propagation.REQUIRED)
     public void saveUserTransactional(SysUser user) {
         sysUserMapper.insert(user);
         int a = 1 / 0;
