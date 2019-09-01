c34fbdbbbdb9bc605bf06336db9ef27a65ea2c0d
bbxyard <bbxyard@gmail.com>
2019-09-01 14:10:27 +0800

    mp-autogen

---
+++ yard/skills/66-java/mp-autogen/src/main/java/com/bbxyard/mpautogen/MpAutogenApplication.java
@@ -0,0 +1,13 @@
+package com.bbxyard.mpautogen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpAutogenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpAutogenApplication.class, args);
+	}
+
+}
