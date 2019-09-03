8371f1d482ac1ecfdb2831311cea38bd24daa803
bbxyard <bbxyard@gmail.com>
2019-09-03 22:19:20 +0800

    mp-demo 调整

--- yard/skills/66-java/mp-demo/src/main/java/com/bbxyard/mp/demo/DemoApplication.java
+++ yard/skills/66-java/mp-demo/src/main/java/com/bbxyard/mp/demo/DemoApplication.java
@@ -1,13 +1,35 @@
 package com.bbxyard.mp.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@Controller
+@EnableWebMvc
 @SpringBootApplication
-public class DemoApplication {
+@MapperScan(basePackages = "tk.mybatis.springboot.mapper")
+public class DemoApplication implements CommandLineRunner {
+
+    private Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("服务启动完成!");
+    }
+
+    @RequestMapping("/")
+    String home() {
+        return "Enjoy it.";
     }
 
 }
