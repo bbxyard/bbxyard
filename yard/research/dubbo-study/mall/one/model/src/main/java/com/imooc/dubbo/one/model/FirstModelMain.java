d04fbcfd6f764a1ead73ce97d15bdf8a70b74ed5
bbxyard <bbxyard@gmail.com>
2019-09-07 01:04:46 +0800

    规范命名

--- yard/research/dubbo-study/mall/one/model/src/main/java/com/imooc/dubbo/one/model/FirstModelMain.java
+++
@@ -1,8 +0,0 @@
-package com.imooc.dubbo.one.model;
-
-public class FirstModelMain {
-
-    public static void main(String[] args) {
-        System.out.println("This is the Dubbo First Model Main");
-    }
-}
