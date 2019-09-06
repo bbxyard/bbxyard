25a65ff1ffb4e99526bb611b10836f2114923e7b
bbxyard <bbxyard@gmail.com>
2019-09-06 23:25:35 +0800

    编译等修正

--- yard/research/dubbo-study/mall/one/server/src/main/java/com/imooc/dubbo/one/server/OneServerApplication.java
+++ yard/research/dubbo-study/mall/one/server/src/main/java/com/imooc/dubbo/one/server/OneServerApplication.java
@@ -1,4 +1,10 @@
 package com.imooc.dubbo.one.server;
 
+
 public class OneServerApplication {
+
+    public static void main(String[] args) {
+
+    }
+
 }
---
+++ yard/research/dubbo-study/mall/two/server/src/main/java/com/imooc/dubbo/two/server/TwoServerApplication.java
@@ -0,0 +1,13 @@
+package com.imooc.dubbo.two.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwoServerApplication.class, args);
+    }
+
+}
