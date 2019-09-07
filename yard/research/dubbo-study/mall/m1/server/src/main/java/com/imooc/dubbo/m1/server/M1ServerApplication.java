84f4f13ed71648a32f831ab51901ce43bad6566e
bbxyard <bbxyard@gmail.com>
2019-09-07 12:30:26 +0800

    基本配置

--- yard/research/dubbo-study/mall/m1/server/src/main/java/com/imooc/dubbo/m1/server/M1ServerApplication.java
+++ yard/research/dubbo-study/mall/m1/server/src/main/java/com/imooc/dubbo/m1/server/M1ServerApplication.java
@@ -2,9 +2,11 @@ package com.imooc.dubbo.m1.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 
 @SpringBootApplication
+@ImportResource(value = {"classpath:spring/spring-jdbc.xml","classpath:spring/spring-dubbo.xml"})
 public class M1ServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(M1ServerApplication.class, args);
