c0beb0180da69a6da32cb63a694625b75f2b11f2
bbxyard <bbxyard@gmail.com>
2019-09-07 13:30:03 +0800

    配置搞晕了

--- yard/research/dubbo-study/mall/m1/server/src/main/java/com/imooc/dubbo/m1/server/M1ServerApplication.java
+++ yard/research/dubbo-study/mall/m1/server/src/main/java/com/imooc/dubbo/m1/server/M1ServerApplication.java
@@ -2,11 +2,10 @@ package com.imooc.dubbo.m1.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 
 
 @SpringBootApplication
-@ImportResource(value = {"classpath:spring/spring-jdbc.xml","classpath:spring/spring-dubbo.xml"})
+// @ImportResource(value = {"classpath:spring/spring-dubbo.xml"})
 public class M1ServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(M1ServerApplication.class, args);
--- yard/research/dubbo-study/mall/m1/server/src/main/java/com/imooc/dubbo/m1/server/controller/BaseController.java
+++ yard/research/dubbo-study/mall/m1/server/src/main/java/com/imooc/dubbo/m1/server/controller/BaseController.java
@@ -4,18 +4,21 @@ package com.imooc.dubbo.m1.server.controller;
 import com.imooc.dubbo.m1.api.enums.StatusCode;
 import com.imooc.dubbo.m1.api.response.BaseResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Slf4j
+@RequestMapping("/base")
 public class BaseController {
 
-    private static final String prefix = "base";
+    // private static final String prefix = "base";
 
     /**
      * 测试hallo
      */
+    @RequestMapping("hallo")
     public BaseResponse hallo(@RequestParam String param) {
         BaseResponse resp = new BaseResponse(StatusCode.Success);
         try {
@@ -24,6 +27,7 @@ public class BaseController {
             e.printStackTrace();
             resp = new BaseResponse(StatusCode.Fail);
         }
+        System.out.println("out: " + resp);
         return resp;
     }
 
