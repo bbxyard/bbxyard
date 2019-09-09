f7b1215a1295bbf6357c457e87d07ba5c2d5e82b
bbxyard <bbxyard@gmail.com>
2019-09-09 11:07:48 +0800

    独立出mp_study

---
+++ yard/skills/66-java/mp-study/mp-quickstart/src/main/java/com/bbxyard/mp/quickstart/MPQuckStartApplication.java
@@ -0,0 +1,13 @@
+package com.bbxyard.mp.quickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MPQuckStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MPQuckStartApplication.class, args);
+    }
+
+}
