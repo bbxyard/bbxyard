242e0a762b7806c8d32750674488c4001d72b75e
bbxyard <bbxyard@gmail.com>
2019-09-13 15:28:57 +0800

    分页

---
+++ yard/skills/66-java/mp-study/mp-pagination/src/main/java/com/bbxyard/mp/pagination/config/MPConfig.java
@@ -0,0 +1,21 @@
+package com.bbxyard.mp.pagination.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MPConfig {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        // 开启 count 的 join 优化,只针对 left join !!!
+        return new PaginationInterceptor().setCountSqlParser(new JsqlParserCountOptimize(true));
+    }
+
+
+}
