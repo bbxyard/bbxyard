05fa8285f57c16cacd792a5473ed96f5fe4e6ff3
bbxyard <bbxyard@gmail.com>
2019-09-27 12:41:13 +0800

    加上Controller可Run

--- yard/skills/66-java/mp-study/mp-auto-gen/src/main/java/com/bbxyard/mp/autogen/controller/AfmUserController.java
+++ yard/skills/66-java/mp-study/mp-auto-gen/src/main/java/com/bbxyard/mp/autogen/controller/AfmUserController.java
@@ -1,9 +1,16 @@
 package com.bbxyard.mp.autogen.controller;
 
 
+import com.bbxyard.mp.autogen.entity.AfmUser;
+import com.bbxyard.mp.autogen.service.AfmUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -13,9 +20,19 @@ import org.springframework.stereotype.Controller;
  * @author bbxyard
  * @since 2019-09-27
  */
-@Controller
+@RestController
 @RequestMapping("/afmUser")
 public class AfmUserController {
+
+    @Autowired
+    private AfmUserService userService;
+
+    @RequestMapping({"/", "index"})
+    public Object listAll() {
+        List<AfmUser> users = userService.list();
+        return users;
+    }
+
 
 }
 
