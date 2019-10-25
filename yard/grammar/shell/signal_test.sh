4272af29f671dbd3bf2d58bc1031865bf7003e07
bbxyard <bbxyard@gmail.com>
2019-10-26 00:26:37 +0800

    rm package-lock.json

--- yard/grammar/shell/signal_test.sh
+++ yard/grammar/shell/signal_test.sh
@@ -1,12 +1,35 @@
 #! /bin/sh
 
-Int14Proc()
-{
+function handle_signal_int() {
+    echo "Recv CTRL - C"
+    exit 2
+}
+
+function handle_signal_kill() {
+    echo "Recv Kill -9"
+    exit 9
+}
+
+function handle_signal_term() {
+    echo "Rec SIGTERM"
+    exit 15
+}
+
+function handle_signal_14() {
     echo hello and make a interaput!! 
     exit 14
 }
 
-trap Int14Proc 14
+function handle_signal_x() {
+    echo "RECV SIGNAL and EXIT"
+    exit -1
+}
+
+# trap handle_signal_kill KILL
+# trap handle_signal_term TERM
+# trap handle_signal_int INT
+trap handle_signal_14 14
+# trap handle_signal_x 14 KILL TERM INT
 
 echo wait a signal...
 echo "my pid is: $$"
