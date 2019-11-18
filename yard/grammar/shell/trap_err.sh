514e6d384fbc10ac2f2bb9a286dc6f439a55cb74
Brian <bbxyard@gmail.com>
2019-11-18 18:09:29 +0800

    try error

--- yard/grammar/shell/trap_err.sh
+++ yard/grammar/shell/trap_err.sh
@@ -3,46 +3,56 @@
 
 function error_handler() {
   errcode=$? # save the exit code as the first thing done in the trap function
-  echo "error $errorcode"
-  echo "the command executing at the time of the error was"
-  echo "$BASH_COMMAND"
-  echo "on line ${BASH_LINENO[0]}"
-  # do some error handling, cleanup, logging, notification
-  # $BASH_COMMAND contains the command that was being executed at the time of the trap
-  # ${BASH_LINENO[0]} contains the line number in the script of that command
-  # exit the script or return to try again, etc.
-  # creating stack...
-  exit $errcode  # or use some other value or do return instead
+  echo "last error code is: $errorcode"
+  echo "[${BASH_LINENO[0]}]: $BASH_COMMAND"
+  exit $errcode
 }
 
 function debug_handler() {
-  echo "Hallo, Das ist debug... $@"
+  echo "[${LINENO}]Hallo, Das ist debug... [${BASH_LINENO[0]}] ${FUNCNAME[1]}"
 }
 
 function return_handler() {
   echo "hooked return value: $?"
+  errcode=$? # save the exit code as the first thing done in the trap function
+  echo "last error code is: $errorcode"
+  echo "[${BASH_LINENO[0]}]: $BASH_COMMAND"
+  exit $errcode
 }
-
-trap error_handler ERR
-trap debug_handler DEBUG
-trap return_handler RETURN
 
 function test_normal() {
-  echo "hey this is a normal functio"
+  echo "hey this is a normal function"
   sleep 1
+  return 2
 }
 
-function test_perm() {
-  touch no-perm.txt && ./no-perm.txt
+function test_raise_an_error() {
+  echo "==> ${LINENO}/${FUNCNAME[0]}"
+  $(touch no-perm.txt && ./no-perm.txt) 
+}
+
+function test_raise_an_error_wrapper() {
+  echo "==> ${LINENO}/${FUNCNAME[0]} RAISE AN ERROR"
+  test_raise_an_error
   ret=$?
+  echo "==> ${LINENO}/${FUNCNAME[0]} CAN NOT SEE THIS"
   rm no-perm.txt
   # exit $ret
   return $ret
 }
 
+# Registry
+trap error_handler ERR
+# trap debug_handler DEBUG
+# trap return_handler RETURN
+
 # 开始实验
-test_normal
-test_perm
+function run_test() {
+  test_raise_an_error
+  test_normal
+  test_normal
+  # test_raise_an_error_wrapper
+}
 
-echo "hey hey. cannot see this line."
-
+run_test
+echo "[MAIN] hey hey. cannot see this line."
