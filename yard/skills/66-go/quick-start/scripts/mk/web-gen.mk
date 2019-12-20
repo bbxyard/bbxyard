22bc37ffe41c4276385318a7125f067d69fa20a9
bbxyard <bbxyard@gmail.com>
2019-12-20 18:19:25 +0800

    smoke

--- yard/skills/66-go/quick-start/scripts/mk/web-gen.mk
+++ yard/skills/66-go/quick-start/scripts/mk/web-gen.mk
@@ -10,12 +10,24 @@ WEB_GEN_BEEGO_NEW_DEMO_PORT := 18802
 # ####################################
 # Dashboard AREA
 # ####################################
-# run-web-mvc: run-web-mvc-beego-normal-case \
-# 	run-web-mvc-gin-normal-case \
-# 	run-web-mvc-iris-normal-case
+run-web-gen: run-web-gen-beego-api-normal-case \
+	run-web-gen-beego-new-normal-case
 
 start-web-gen-beego-api:
 	cd $(SRC_DIR)/web-gen/beego-api-demo && go run main.go
 
 start-web-gen-beego-new:
 	cd $(SRC_DIR)/web-gen/beego-new-demo && go run main.go
+
+
+# ####################################
+# Beego-api AREA
+# ####################################
+run-web-gen-beego-api-normal-case:
+	curl localhost:${WEB_GEN_BEEGO_API_DEMO_PORT}
+
+# ####################################
+# Beego-new AREA
+# ####################################
+run-web-gen-beego-new-normal-case:
+	curl localhost:${WEB_GEN_BEEGO_NEW_DEMO_PORT}
