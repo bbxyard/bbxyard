e6e1f0fcd2372052994cb268aefc61cf9651a85e
bbxyard <bbxyard@gmail.com>
2020-01-05 20:43:19 +0800

    adjust sql make scripts

--- yard/skills/99-imooc/go-iris-superstar/stub/db-superstar-baseline.sql
+++ yard/skills/99-imooc/go-iris-superstar/stub/db-superstar-baseline.sql
@@ -13,9 +13,9 @@ MySQL - 5.5.46-0ubuntu0.14.04.2 : Database - superstar
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`superstar` /*!40100 DEFAULT CHARACTER SET utf8 */;
+-- CREATE DATABASE /*!32312 IF NOT EXISTS*/`go_iris_superstar` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
-USE `superstar`;
+USE `go_iris_superstar`;
 
 /*Table structure for table `star_info` */
 
--- yard/skills/99-imooc/go-iris-superstar/stub/db.mk
+++ yard/skills/99-imooc/go-iris-superstar/stub/db.mk
@@ -1,7 +1,7 @@
 # ####################################
 # 数据库脚本 AREA
 # ####################################
-STAR_DB_NAME	:= go_superstar
+STAR_DB_NAME	:= go_iris_superstar
 STAR_DB_CMD_CREATE := CREATE DATABASE IF NOT EXISTS ${STAR_DB_NAME} DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 STAR_INIT_SQL_FILE := $(CURDIR)/go-iris-superstar/stub/db-superstar-baseline.sql
 
